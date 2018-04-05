49d58ce9d71ef7cb94ea5b2e0d8c67c6b9f8eaaf
Melissa Strab <mstrab@udel.edu>
2018-04-05 10:37:06 -0400

    JavaScript II Exercise

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,10 @@
+let blist = ["end of the semester", "no research", "Disen gets better"];
+console.log(blist.length);
+blist.push("get a beer");
+console.log(blist.length);
+console.log(blist);
+
+// for loop
+for (let i=0; i<blist.length; i++) {
+  console.log(blist[i]);
+}
