90beb9b0f1b4ca6abcf81d24c3eb5c6d49b062a7
Melissa Strab <mstrab@udel.edu>
2018-04-19 10:23:37 -0400

    add exercises

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "hello"
+name = "Missy"
+print greeting  # this print out string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %s!" % (greeting, name)
