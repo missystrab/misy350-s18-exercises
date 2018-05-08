6cedc33f64a7599a40c33cddb224774e0bfcaa90
Melissa Strab <mstrab@udel.edu>
2018-05-08 10:40:13 -0400

    Database Added

--- songbase/manage.py
+++ songbase/manage.py
@@ -1,8 +1,12 @@
 from flask_script import Manager
-from songbase import app
+from songbase import app, db, Artist
 
 manager = Manager(app)
 
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
 
 if __name__ == '__main__':
     manager.run()
--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,6 +1,20 @@
+import os
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
 
 @app.route('/')
 def index():
